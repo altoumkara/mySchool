df16059b1958bca363f1e1178f70c6d96ec81e82
alama <alama91579@hotmail.com>
2015-06-03 21:43:51 -0400

    Added the git deployment script

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once('functions/layout.php');
+create_header("Sign-up", "assets/stylesheets/MyschoolsignupCss.css");
+create_header_login();
+create_signup_form();
+create_footer();
+
+?>
